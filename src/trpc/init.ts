1402bbc3f622b89d6a42e65eb1311458cbf51164
Speculate Dev <sida3li777@gmail.com>
2025-08-02 20:27:31 +0100

    TRPC-payload: extended procedure as to not repeat getPayload

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,3 +1,6 @@
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+
 import { initTRPC } from "@trpc/server";
 import { cache } from "react";
 export const createTRPCContext = cache(async () => {
@@ -21,4 +24,10 @@ const t = initTRPC.create({
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
-export const baseProcedure = t.procedure;
+export const baseProcedure = t.procedure.use(async ({ next }) => {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  return next({ ctx: { db: payload } });
+});
