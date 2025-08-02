143ac16e3e1739919cd8c382dcc61f8e6362834f
Speculate Dev <sida3li777@gmail.com>
2025-08-02 23:14:43 +0100

    TRPC: infer output type instead of CustomCategories

---
+++ src/modules/categories/types.ts
@@ -0,0 +1,7 @@
+import { inferRouterOutputs } from "@trpc/server";
+import { AppRouter } from "@/trpc/routers/_app";
+
+export type CategoriesGetManyOutput =
+  inferRouterOutputs<AppRouter>["categories"]["getMany"];
+
+export type CategoriesGetManyOutputSingle = CategoriesGetManyOutput[0];
