1b5ce6ddd8c968bf2a1dd7f76ce20696b41e7c06
Speculate Dev <sida3li777@gmail.com>
2025-08-04 15:43:34 +0100

    09: session check redirection

--- src/app/(app)/(auth)/login/page.tsx
+++ src/app/(app)/(auth)/login/page.tsx
@@ -1,5 +1,13 @@
 import SignInView from "@/modules/auth/ui/views/sign-in-view";
+import { caller } from "@/trpc/server";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await caller.auth.session();
+
+  if (session.user) {
+    redirect("/");
+  }
+
   return <SignInView />;
 }
