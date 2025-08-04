bacf8effacf7734050650c80749471de6118a9e6
Speculate Dev <sida3li777@gmail.com>
2025-08-04 16:18:17 +0100

    09: auth-states

--- src/app/(app)/(home)/search-filters/SubcategoryMenu.tsx
+++ src/app/(app)/(home)/search-filters/SubcategoryMenu.tsx
@@ -1,6 +1,7 @@
 import { Category } from "@/payload-types";
 import Link from "next/link";
 import { CategoriesGetManyOutput } from "@/modules/categories/types";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface Props {
   category: CategoriesGetManyOutput[1];
@@ -27,22 +28,24 @@ function SubcategoryMenu({ category, isOpen, position }: Props) {
       {/* Maintain hover state between the menu and the button */}
       <div className="h-3 w-60" />
 
-      <div
-        style={{ backgroundColor: backgroundColor }}
-        className="w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]"
-      >
-        <div>
-          {category.subcategories?.map((subcategory: Category) => (
-            <Link
-              key={subcategory.slug}
-              href={`/${category.slug}/${subcategory.slug}`}
-              className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center underline font-medium"
-            >
-              {subcategory.name}
-            </Link>
-          ))}
+      <ScrollArea className="flex max-h-[60vh] flex-col overflow-y-auto w-auto border rounded-md">
+        <div
+          style={{ backgroundColor: backgroundColor }}
+          className="w-60 text-black overflow-hidden shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]"
+        >
+          <div>
+            {category.subcategories?.map((subcategory: Category) => (
+              <Link
+                key={subcategory.slug}
+                href={`/${category.slug}/${subcategory.slug}`}
+                className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center underline font-medium"
+              >
+                {subcategory.name}
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
+      </ScrollArea>
     </div>
   );
 }
