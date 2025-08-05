c7f23a8fc99589328ebd62d214576deb2558bb32
Speculate Dev <sida3li777@gmail.com>
2025-08-05 14:35:00 +0100

    11: get products route

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,12 @@
 import { createTRPCRouter } from "../init";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { authRouter } from "@/modules/auth/server/procedures";
+import { productsRouter } from "@/modules/products/server/procedures";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   categories: categoriesRouter,
+  products: productsRouter,
 });
 
 // export type definition of API
