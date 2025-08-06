9838de7ddc2222b4c5077a000c37c4ee870bf4ed
Speculate Dev <sida3li777@gmail.com>
2025-08-06 14:44:53 +0100

    12: filtering products

---
+++ src/modules/products/ui/components/products-filter.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { ChevronDownIcon, ChevronRightIcon } from "lucide-react";
+import { useState } from "react";
+import PriceFilter from "./price-filter";
+import useProductsFilters from "../../hooks/use-products-filters";
+
+interface ProductFilterProps {
+  title: string;
+  className?: string;
+  children: React.ReactNode;
+}
+
+const ProductFilter = ({ title, className, children }: ProductFilterProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [filters, setFilters] = useProductsFilters();
+
+  const Icon = isOpen ? ChevronDownIcon : ChevronRightIcon;
+
+  return (
+    <div className={cn("p-4 border-b flex flex-col gap-2", className)}>
+      <div
+        className="flex items-center justify-between cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <p className="font-medium">{title}</p>
+        <Icon className="size-5" />
+      </div>
+      {isOpen && children}
+    </div>
+  );
+};
+
+function ProductsFilter() {
+  const [filters, setFilters] = useProductsFilters();
+
+  const onChange = (key: keyof typeof filters, value: unknown) => {
+    setFilters({ ...filters, [key]: value });
+  };
+
+  return (
+    <div className="border rounded-md bg-white">
+      <div className="p-4 border-b flex items-center justify-between">
+        <p className="font-medium">Filters</p>
+        <button className="underline" type="button" onClick={() => {}}>
+          Clear
+        </button>
+      </div>
+
+      <ProductFilter title="Price" className="border-b-0">
+        <PriceFilter
+          minPrice={filters.minPrice}
+          maxPrice={filters.maxPrice}
+          onMinPriceChange={(value) => onChange("minPrice", value)}
+          onMaxPriceChange={(value) => onChange("maxPrice", value)}
+        />
+      </ProductFilter>
+    </div>
+  );
+}
+
+export default ProductsFilter;
